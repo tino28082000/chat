718651f58d15ce67a3f0f978b32c0f783b4ae379
Weishow <tino28082000@gmail.com>
2020-11-02 12:36:33 +0800

    Refactor

--- chat.py
+++ chat.py
@@ -1,25 +1,32 @@
 chat_new = []
-person = None
+def convert(filename):
+	with open(filename,'r', encoding = 'utf-8') as i:
+		person = None
+		for line in i:
+			if '你傳送的時間：' in line:
+				person = 'Tino:'
+				continue
+			elif '傳送的時間：' in line:
+				continue
+			elif '前' in line:
+				continue
+			elif '媽的老頭 1.75w' in line:
+				person = '俊凱:'
+				continue
+			if person:
+				chat_new.append(person + line)
+		return chat_new
 
-with open('input.txt','r', encoding = 'utf-8') as i:
-	for line in i:
-		if '你傳送的時間：' in line:
-			person = 'Tino:'
-			continue
-		elif '傳送的時間：' in line:
-			continue
-		elif '前' in line:
-			continue
-		elif '媽的老頭 1.75w' in line:
-			person = '俊凱:'
-			continue
-		if person:
-			chat_new.append(person + line)
-print(chat_new)
+def write(filename, message):
+	with open(filename,'w', encoding = 'utf-8') as o:
+		for line in message:
+			o.write(line)
 
-with open('output.txt','w', encoding = 'utf-8') as o:
-	for line in chat_new:
-		o.write(line)
+new_message = convert('input.txt')
+print(new_message)
+write('output.txt',new_message)
+
+
 
 
 
