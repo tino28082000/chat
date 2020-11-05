6bca61c6f375ff12650eb987d9020d2ec0dfff64
Weishow <tino28082000@gmail.com>
2020-11-05 09:39:16 +0800

    update

--- chat.py
+++ chat.py
@@ -14,15 +14,23 @@ def convert(lines):
 	count_jimmy = 0
 	for line in lines:
 		s = line.strip().split(' ')
+		time = s[0]
 		name = s[1]
 		content = s[2:]
-		new_message.append(str(name) + str(content))
+		if ':' not in time:
+			new_message.append('今天是' + str(time))
 		if name == 'Tino.L':
 			for m in content:
 				count_tino += len(m)
 		elif name == 'Jimmy':
 			for m in content:
 				count_jimmy += len(m)
+		if '星期' in name:
+			new_message.append(str(name))
+		else :
+			new_message.append(str(name) + str(content))
+
+
 	print(count_tino, count_jimmy)
 	print(new_message)
 	return new_message
@@ -33,11 +41,11 @@ def write(filename, message):
 		for line in message:
 			o.write(str(line) + '\n')
 
+
 def main(inputFileName,outputFileName): 
 	chat_new = openfile(inputFileName)
 	new_message = convert(chat_new)
 	write(outputFileName,new_message)
-
 
 
 main('[LINE]Jimmy.txt','linechat.txt')
